441cec68e472b2f913be4ae98f6b09c35aec930c
Prantar <prantarbhowmik@yahoo.com>
2018-06-16 01:51:04 -0400

    Table connected to db
    TODO: connect without hardcoding

--- src/app/homepage/table-page/table-page.component.ts
+++ src/app/homepage/table-page/table-page.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
-// import { AuthService } from '../../core/auth.service';
-// import { JobsService } from '../../core/jobs.service';
-// import { AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
-// import { DataSource } from '@angular/cdk/collections';
-// import { Observable } from 'rxjs';
+import { JobsService } from '../../core/jobs.service';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
 export interface JobData {
@@ -21,29 +17,19 @@ export interface JobData {
 })
 export class TablePageComponent {
   displayedColumns = ['companyName', 'location', 'salary'];
-  dataSource: MatTableDataSource<JobData>;
+  dataSource: MatTableDataSource<{}>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  users: JobData[] = [];
-
-  constructor() {
-    // Create 100 users
-    // const users: JobData[] = [];
-    // for (let i = 1; i <= 50; i++) { users.push(createNewUser(i)); }
-
+  constructor(private jobsService: JobsService) {
     // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(this.users);
-  }
-
-  /**
-   * Set the paginator and sort after the view init since this component will
-   * be able to query its view for the initialized paginator and sort.
-   */
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    // Set the paginator and sort
+    this.jobsService.getJobs('SOYzrrmi7revQ6TO5SG7rC8m4mE2').subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
   }
 
   applyFilter(filterValue: string) {
@@ -52,37 +38,3 @@ export class TablePageComponent {
     this.dataSource.filter = filterValue;
   }
 }
-
-/** Builds and returns a new User. */
-function createNewUser(jobId: number): JobData {
-  const companyName = 'Google';
-  const location = 'NY';
-  const salary = 1;
-
-  return {
-    jobId: jobId,
-    companyName: companyName,
-    location: location,
-    salary: salary
-  };
-}
-
-
-/**  Copyright 2018 Google Inc. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at http://angular.io/license */
-
-// export class JobDataSource extends DataSource<any> {
-//
-//   constructor(private jobsService: JobsService, private userId: String){
-//     super();
-//   }
-//
-//   connect() {
-//     return this.jobsService.getJobs(this.userId);
-//   }
-//
-//   disconnect() {
-//
-//   }
-// }
